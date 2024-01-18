42526178eb7ea61785a3d133d7451dc8f5c3e398
NikiSukiasyan <justplayingcsgo@gmail.com>
2024-01-18 11:43:39 +0400

    JS Example

---
+++ script.js
@@ -0,0 +1,95 @@
+// let user = 17;
+
+// if (user < 18) {
+//     console.log("ვერა")
+// } else {
+//     console.log("გამარჯოვა")
+// }
+
+// let input = prompt("შეიყვანე რიცხვი");
+
+// if (input.length === 2) {
+//     console.log("ornishna")
+// } else {
+//     console.log("rame")
+// }
+
+// let weekday = prompt("enter weekday");
+
+// switch (parseInt(weekday)) {
+//   case 1:
+//     console.log("orshabati");
+//     break;
+//   case 2:
+//     console.log("samshabati");
+//     break;
+//   case 3:
+//     console.log("otxshabati");
+//     break;
+//   default:
+//     console.log("error");
+//     break;
+// }
+
+// 5 ფაქტორიალია 1 * 2 * 3 * 4 * 5 = 120
+
+// function factorial(num) {
+//   if (num === 0 || num === 1) {
+//     return 1;
+//   } else {
+//     return num * factorial(num - 1);
+//   }
+// }
+
+// console.log(factorial(5));
+
+// function timer() {
+//   let number = 10;
+
+//   setInterval(() => {
+//     if (number === 0) {
+//       clearInterval();
+//     } else {
+//       console.log(number);
+//       number--;
+//     }
+//   }, 1000);
+// }
+
+// timer();
+// // = == ===
+
+// const counter = () => {
+//   let number = 0;
+
+//   setInterval(() => {
+//     if (number >= 10) {
+//       clearInterval();
+//     } else {
+//       console.log(number);
+//       number++;
+//     }
+//   }, 1000);
+// };
+
+// // counter();
+// // function timer() {}
+// // const timer = () => {};
+
+// // setTimeout(() => {
+// //  console.log("hello")
+// // }, 5000);
+
+// // setInterval(() => {
+// //   console.log("hello lana");
+// // }, 2000);
+
+// // let name = "lana ";
+
+// // console.log(name.length);
+
+// // let input = prompt("enter something");
+
+// // if () {
+// // } else {
+// // }
